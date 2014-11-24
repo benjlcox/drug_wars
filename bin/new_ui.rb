0ba4bc586bef1b3ee9e53dbc8494c529b6d39e87
Ben Cox <ben@shopify.com>
2014-11-24 12:44:35 -0500

    Major refactor of UI

---
+++ bin/new_ui.rb
@@ -0,0 +1,56 @@
+class NewUI
+
+  def initialize(player)
+    @player = player
+  end
+
+  def start
+    @current_screen = TravelScreen.new(@player)
+
+    loop do
+      @current_screen.paint
+      user_input = gets.chomp
+      break if user_input == "quit"
+      handle_user_input(user_input)
+    end
+
+  end
+
+  private
+
+  def handle_user_input(user_input)
+    if @current_screen.allow_global_actions?
+      success = handle_global_action(user_input)
+    end
+
+    return if success
+
+    if @current_screen.respond_to?(:handle_action)
+      @current_screen.handle_action(user_input)
+    else
+      print "Invalid input. Try again."
+      sleep 1.5
+    end
+  end
+
+  def handle_global_action(input)
+    case input
+    when "1"
+      @current_screen = MarketplaceScreen.new(@player)
+      true
+    when "2"
+      # inventory_screen
+    when "3"
+      @current_screen = TravelScreen.new(@player)
+      true
+    when "4"
+      # loan_screen
+    when "5"
+      # fence_screen
+    else
+      return false
+    end
+    return true
+  end
+
+end
