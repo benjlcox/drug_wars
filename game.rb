2bc573d104a3374acfee51ab13ce1ee005cc3248
Ben Cox <ben@shopify.com>
2014-11-24 12:42:31 -0500

    Add game.rb

---
+++ game.rb
@@ -0,0 +1,20 @@
+require 'pry'
+require 'yaml'
+require 'byebug'
+
+require_relative "bin/drug"
+require_relative "bin/city"
+require_relative "bin/world"
+require_relative "bin/backpack"
+require_relative "bin/wallet"
+require_relative "bin/player"
+require_relative "bin/new_ui"
+require_relative "bin/screen"
+require_relative "bin/screens/travel_screen"
+require_relative "bin/screens/marketplace_screen"
+#require_relative "bin/ui"
+
+game = NewUI.new(Player.new("Ben", "Toronto"))
+game.start
+# player = Player.new("Ben", "Toronto")
+# binding.pry
