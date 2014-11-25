92f10341d310f7c1cd96b4358665d761c1b58106
Ben Cox <ben@shopify.com>
2014-11-24 21:04:33 -0500

    Require all files automatically

--- game.rb
+++ game.rb
@@ -2,22 +2,9 @@ require 'pry'
 require 'yaml'
 require 'byebug'
 
-require_relative "bin/drug"
-require_relative "bin/city"
-require_relative "bin/world"
-require_relative "bin/backpack"
-require_relative "bin/wallet"
-require_relative "bin/player"
-require_relative "bin/ui"
-require_relative "bin/screen"
-require_relative "bin/screens/travel_screen"
-require_relative "bin/screens/marketplace_screen"
-require_relative "bin/screens/inventory_screen"
-require_relative "bin/screens/buy_screen"
-require_relative "bin/screens/sell_screen"
-require_relative "bin/screens/transaction_screen"
+Dir[Dir.pwd + "/**/*.rb"].each { |file| require file }
 
-game = NewUI.new(Player.new("Ben", "Toronto"))
+game = UI.new(Player.new("Ben", "Toronto"))
 game.start
 #player = Player.new("Ben", "Toronto")
 #binding.pry
